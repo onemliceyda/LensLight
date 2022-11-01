e04f094f060214482db472c79337381e57e6bbfd
Ceyda <ceydaonemli2@gmail.com>
2022-11-01 20:23:25 +0300

    model view controller

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 import express from "express"
+import dotenv from "dotenv"
+import conn from "./db.js"
+import pageRoute from "./routes/pageRoute.js"
+
+
+dotenv.config()
+
+//connection to the DB
+conn()
+
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
 //ejs template engine
 app.set("view engine", "ejs")
+
 //static files middleware
 app.use(express.static("public"))
 
-app.get("/", (req, res) => {
-  res.render("index")
-}) //routing with get
+//routes
+app.use("/",pageRoute)
 
-app.get("/about", (req, res) => {
-  res.render("about")
-})
 
 app.listen(port, () => {
   console.log(`Application running on port : ${port}`)
