7a17a2b16dd7e826cb1700b6cee711ad2c66f153
Ceyda <ceydaonemli2@gmail.com>
2022-11-01 15:48:14 +0300

    template engine,static files and ejs

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 import express from "express"
-const app=express()
-const port=3000
+const app = express()
+const port = 3000
 
-app.get("/",(req,res)=>{
-    res.send("Index Page 2 ")
+//ejs template engine
+app.set("view engine", "ejs")
+//static files middleware
+app.use(express.static("public"))
+
+app.get("/", (req, res) => {
+  res.render("index")
 }) //routing with get
 
-app.listen(port,()=>{
-    console.log(`Application running on port : ${port}`);
+app.get("/about", (req, res) => {
+  res.render("about")
+})
+
+app.listen(port, () => {
+  console.log(`Application running on port : ${port}`)
 })
